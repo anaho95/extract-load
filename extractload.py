985467a67c298e5b7a6fd914f38697e67e95cfd1
Ana Ho <58791527+anaho95@users.noreply.github.com>
2021-06-29 20:39:59 -0500

    Add files via upload
    An extract and load Python program using PostgreSQL. This program will extract two tables from the the operations database and then load the dataset into the python database.

---
+++ extractload.py
@@ -0,0 +1,31 @@
+# import needed libraries
+import petl as etl
+import psycopg2 as p
+from sqlalchemy import *
+
+# declare connection properties within the data dictionary
+dbCons = {'operations': 'dbname=operations user=postgres password=password host=127.0.0.1',
+          'python': 'dbname=python user=postgres password=password host=127.0.0.1'}
+
+# set connections and cursors
+sourceCon = p.connect(dbCons['operations'])  # grab value by referencing key dictionary
+targetCon = p.connect(dbCons['python'])
+sourceCursor = sourceCon.cursor()
+targetCursor = targetCon.cursor()
+
+# extract the names of the source tables to be copied
+sourceCursor.execute("""select table_name from information_schema.columns where table_name in ('salesperson',
+'returns') group by 1""")
+sourceTables = sourceCursor.fetchall()
+
+
+# iterate through table names to load into python database
+for t in sourceTables:
+    targetCursor.execute("drop table if exists %s" % (t[0]))
+    sourceDataset = etl.fromdb(sourceCon, 'select * from %s' % (t[0]))
+    etl.todb(sourceDataset, targetCon, t[0], create=True, sample=10000)
+
+
+
+
+
